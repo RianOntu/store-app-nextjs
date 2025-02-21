7e2a78656682afbc27ca1d717733fbdd922f17b5
RianOntu <rianontu13@gmail.com>
2025-02-21 19:07:27 +0600

    fixed responsive issue

--- app/components/EmailField.js
+++ app/components/EmailField.js
@@ -27,7 +27,7 @@ const EmailField = ({ register, errors }) => (
             },
           })}
           placeholder="you@example.com"
-          className={`mt-2 w-full md:w-[393.88px] px-4 py-2 border rounded-md focus:ring-2 focus:ring-purple-400 ${
+          className={`mt-2 w-[311px] md:w-[393.88px] px-4 py-2 border rounded-md focus:ring-2 focus:ring-purple-400 ${
             errors.storeName ? "border-red-500" : "border-gray-300"
           }`}
         />
